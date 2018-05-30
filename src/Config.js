4e0965cc78abbc4bf68658dab67466fc8dea03c1
Chetta Crobkatoke <chetta.c@wallstreetenglish.in.th>
2018-05-30 22:01:38 +0700

    initial commit

---
+++ src/Config.js
@@ -0,0 +1,8 @@
+export const Config = {
+    apiKey: "AIzaSyCpIEJwcyxo1b9yFIsFPROJCayaW_j6DOg",
+    authDomain: "react-chat-84ffa.firebaseapp.com",
+    databaseURL: "https://react-chat-84ffa.firebaseio.com",
+    projectId: "react-chat-84ffa",
+    storageBucket: "react-chat-84ffa.appspot.com",
+    messagingSenderId: "710299747990"
+};
